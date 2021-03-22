10130442ba23c72975dc244462c8973e0d75aa94
blázs <64538277+blazsmaster@users.noreply.github.com>
2021-03-22 13:38:25 +0100

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,32 @@
+// configuration
+const Discord = require('discord.js');
+const config = require('../config.json');
+
+// export command
+module.exports = {
+    
+    // command name
+	name: 'help',
+
+    // command description
+	description: 'List all commands.',
+
+    // command
+	execute(message) {
+
+        // const commands
+		const { commands } = message.client;
+
+        // send message to channel
+        message.channel.send(
+
+            // embed
+            new Discord.MessageEmbed()
+            .setColor(config.color.default)
+            .setTitle('Command help')
+            .setDescription(commands.map(c => `\`${config.prefix}${c.name}\` - \`${c.description ? c.description : 'No description provided!'}\``).join('\n'))
+            .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true, size: 64 }))
+            .setTimestamp()
+        );
+	},
+};
