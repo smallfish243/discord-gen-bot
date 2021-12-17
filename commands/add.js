b83e7a65dd5a4eb04fe595da78024083ec8b207f
blazsmaster <64538277+blazsmaster@users.noreply.github.com>
2021-12-17 19:29:02 +0100

    Update add.js

--- commands/add.js
+++ commands/add.js
@@ -1,98 +1,63 @@
-// npmjs packages
+// packages
 const Discord = require('discord.js');
 const fs = require('fs');
 const os = require('os');
-
-// configuration
 const config = require('../config.json');
+const log = new CatLoggr();
 
-// export command
+const CatLoggr = require('cat-loggr');
+
 module.exports = {
-    
-    // command name
-	name: 'add',
+	name: 'add', // command name at execute (may be different from the file name)
+	description: 'Add an account to a specified service.', // description in help command
 
-    // command description
-	description: 'Add an account to a specified service.',
-
-    // command
-	execute(message) {
-
-        // split message content
-        const messageArray = message.content.split(' ');
-
-        // args
-        const args = messageArray.slice(1);
-
-        // service
+    // the command :D
+	execute(message, args) {
+        // constants (parameters)
         const service = args[0];
-
-        // account
         const account = args[1];
 
-        // if no service
+        // if no service parameter specified in the command
         if (!service) {
-
-            // send message to channel
-            message.channel.send(
-
-                // embed
+            return message.channel.send(
                 new Discord.MessageEmbed()
                 .setColor(config.color.red)
-                .setTitle('Missing parameters')
-                .setDescription('You need to give a service name!')
+                .setTitle('Missing parameters!')
+                .setDescription('You need to specify a service!')
+                .addField('For example', `${config.prefix}${this.name} **tree** apple`)
                 .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true, size: 64 }))
                 .setTimestamp()
             );
-
-            // cancel
-            return;
+            // YOU ARE NEED THE RETURN STATEMENT TO STOP THIS FUNCTION (if you remove the "return", all of the functions below will be executed and breaks the command)
         };
 
-        // if no account
+        // if no account parameter specified in the command
         if (!account) {
-
-            // send message to channel
-            message.channel.send(
-
-                // embed
+            return message.channel.send(
                 new Discord.MessageEmbed()
                 .setColor(config.color.red)
-                .setTitle('Missing parameters')
-                .setDescription('You need to give an account or data!')
+                .setTitle('Missing parameters!')
+                .setDescription('You need to specify an account!')
+                .addField('For example', `${config.prefix}${this.name} tree **apple**`)
                 .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true, size: 64 }))
                 .setTimestamp()
-            );
-
-            // cancel
-            return;
+            );            
         };
 
-        // stock files path
-        const filePath = `${__dirname}/../stock/${args[0]}.txt`;
+        const filePath = `${__dirname}/../stock/${args[0]}.txt`; // filepath to the service file
 
-        // file append
+        // append data to file
         fs.appendFile(filePath, `${os.EOL}${args[1]}`, function (error) {
+            if (error) return log.error(error); // if an error occured say to console the error
 
-            // if error
-            if (error) {
-                
-                // write to console
-                console.log(error);
-
-                // cancel
-                return;
-            };
-
-            // send message to channel
             message.channel.send(
                 new Discord.MessageEmbed()
                 .setColor(config.color.green)
                 .setTitle('Account added!')
-                .setDescription(`Successful added \`${args[1]}\` to \`${args[0]}\` service!`)
+                .setDescription(`Successfuly added \`${args[1]}\` account to \`${args[0]}\` service!`)
                 .setFooter(message.author.tag, message.author.displayAvatarURL())
                 .setTimestamp()
-            );
+            ).then(message => message.delete({ timeout: 5000 })); // delete message after 5 seconds
         });
-    },
+    }
 };
