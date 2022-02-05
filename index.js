ed6f3667a6d5de376cad7c0ea14d50348767f40f
blazsmaster <64538277+blazsmaster@users.noreply.github.com>
2022-02-05 17:23:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,54 @@
-// packages
+// Dependencies
 const Discord = require('discord.js');
 const fs = require('fs');
 const config = require('./config.json');
 const CatLoggr = require('cat-loggr');
 
+// Functions
 const client = new Discord.Client();
 const log = new CatLoggr();
 
+// New discord collections
 client.commands = new Discord.Collection();
 
-// logs
+// Logging
 if (config.debug === true) client.on('debug', stream => log.debug(stream)); // if debug is enabled in config
 client.on('warn', message => log.warn(message));
 client.on('error', error => log.error(error));
-client.on('rateLimit', message => log.warn(JSON.stringify(message, null, 2)));
 
-// load commands
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js')); // command library
+// Load commands from folder
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js')); // Command directory
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`); // read commands
-    log.init(`Loaded command ${file.split('.')[0] === command.name ? file.split('.')[0] : `${file.split('.')[0]} as ${command.name}`}`); // say to console
-	client.commands.set(command.name, command); // set commands to use
+	const command = require(`./commands/${file}`); // Load the command
+    log.init(`Loaded command ${file.split('.')[0] === command.name ? file.split('.')[0] : `${file.split('.')[0]} as ${command.name}`}`); // Logging to console
+	client.commands.set(command.name, command); // Set command by name to the discord "commands" collection
 };
 
-// login with token
+// Client login
 client.login(config.token);
 
-// on login
 client.once('ready', () => {
-	log.info(`I am logged in as ${client.user.tag} to Discord!`); // say to console
-    client.user.setActivity(`${config.prefix}help • ${client.user.username.toUpperCase()}`, { type: "LISTENING" }); // set discord bot activity
-    /* you can change the activity type to:
+	log.info(`I am logged in as ${client.user.tag} to Discord!`); // Say hello to console
+    client.user.setActivity(`${config.prefix}help • ${client.user.username.toUpperCase()}`, { type: "LISTENING" }); // Set the bot's activity status
+    /* You can change the activity type to:
      * LISTENING
      * WATCHING
      * COMPETING
-     * STREAMING (you need to add a twitch.ty url next to type like this:   {type:"STREAMING", url:"https://twitch.tv/usernamehere"}   )
+     * STREAMING (you need to add a twitch.tv url next to type like this:   { type: "STREAMING", url: "https://twitch.tv/twitch_username_here"} )
      * PLAYING (default)
     */
 });
 
-// message event // command handling
+// Discord message event and command handling
 client.on('message', (message) => {
-	if (!message.content.startsWith(config.prefix)) return; // if the message does not start with the prefix
-	if (message.author.bot) return; // if a bot tries to execute the command (timed commands by other bots or bot client) (you can remove this line if you want)
+	if (!message.content.startsWith(config.prefix)) return; // If the message does not start with the prefix 
+	if (message.author.bot) return; // If a command executed by a bot
 
-	const args = message.content.slice(config.prefix.length).trim().split(/ +/); // split the messages to arguments
+    // Split message content to arguments
+	const args = message.content.slice(config.prefix.length).trim().split(/ +/);
 	const command = args.shift().toLowerCase();
 
-    // if the command not found
+    // If the command does not exists
 	if (config.command.notfound_message === true && !client.commands.has(command)) {
         return message.channel.send(
             new Discord.MessageEmbed()
@@ -59,13 +60,13 @@ client.on('message', (message) => {
         );
     };
 
-    // execute the command (with error handling)
+    // Executing the command
 	try {
-		client.commands.get(command).execute(message, args); // execute the command
+		client.commands.get(command).execute(message, args); // Execute
 	} catch (error) {
-		log.error(error); // say to console the error
+		log.error(error); // Logging if error
 
-        // send message to channel if error message is enabled in config
+        // Send error messsage if the "error_message" field is "true" in the configuration
 		if (config.command.error_message === true) {
             message.channel.send(
                 new Discord.MessageEmbed()
